5c20428de65453f5f9edda841ec6730d538a5048
Tip Powell <powelltip@gmail.com>
2021-01-29 21:39:01 -0500

    1-3 done, working on 4

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log('Add validation!');
+let price = 5
+let fullPrice =
+
+
+window.addEventListener('submit', event => {
+    event.preventDefault()
+})
+
+
+document.addEventListener('click', validate)
+
+form.addEventListener('click', validate)
+
+
+function validate(event) {
+    
+    parkingformIsValid = true
+    confirmValidForm()
+}
+
+function confirmValidForm() {
+    // if (formIsValid && form.checkValidity()) {
+        if (true) {
+            const validMsgEl = document.querySelector('#total')
+            //creating variable to hold the selector for id total
+            validMsgEl.innterText = totalExpense()
+            //creating a variable to hold the selector for price variable
+        }
+    }
+
+
+function totalExpense () {
+    const totalDays = document.querySelector('#days').Value// const totaldays = document.querySelector('#days'
+    let fullprice = totaldays * price
+    return "Total Cost: $"+ fullprice
+}
+
+
+//line 11 
